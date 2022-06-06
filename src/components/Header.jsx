b73219ce22a3e769f42bca8f27b033039b3ba534
KennethGalvez <gal20079@uvg.edu.gt>
2022-06-06 01:43:37 -0600

    Update

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './consola.css';
+
+export default function Header (){
+    return (
+        <div className="gameboy">
+          <div className='top'>
+            <div className='onoff'>
+              <div className='arrow-left'></div>
+              <div className='arrow-right'></div>
+            </div>
+          </div>
+          <div className='components'>
+            <div className='screen'>
+              <div className='display'></div>
+            </div>
+            <div className='controls'>
+              <div className='inputs'>
+                <div className='dpad'>
+                  <div className='left'></div>
+                  <div className='up'></div>
+                  <div className='right'></div>
+                  <div className='down'></div>
+                </div>
+                <div className='buttons'>
+                  <div className='button-b'></div>
+                  <div className='button-a'></div>
+                </div>
+                <div className='selections'>
+                  <div className='select'></div>
+                  <div className='start'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      )
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+export default {
+    mode: 'development',
+	entry:'./src/index.js',
+	output:{
+	  filename: 'bundle.js',
+  },
+  devServer:{
+
+	static: {
+	 directory: 'dist',
+	},
+	compress: true,
+	port: 9000,
+  },
+  module: {
+	rules: [{ 
+		test: /\.js$|jsx/,
+		use: ['babel-loader']
+	},
+	{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+    },
+	],
+  },
+}
